708d2bdb501433ba87067ce65c376e54e253279d
greenjoe <joegreen@joegreen.pl>
2017-02-22 23:30:04 +0100

    Additional doc in Actions

--- src/main/java/pl/joegreen/sergeants/framework/Actions.java
+++ src/main/java/pl/joegreen/sergeants/framework/Actions.java
@@ -4,12 +4,20 @@ import pl.joegreen.sergeants.api.GeneralsApi;
 import pl.joegreen.sergeants.framework.model.GameState;
 
 public interface Actions {
+
     void move(int indexFrom, int indexTo);
 
+    /**
+     * Direct move between fields. Works only if both fields are neighbours (either horizontally or vertically, not diagonally) - otherwise does nothing and logs error.
+     */
     void move(GameState.Field fieldFrom, GameState.Field fieldTo);
 
     void move(int indexFrom, int indexTo, boolean moveHalf);
 
+    /**
+     * Direct move between fields. Works only if both fields are neighbours (either horizontally or vertically, not diagonally) - otherwise does nothing and logs error.
+     * @param moveHalf if true, only half of the army will be moved (equal to double click in browser)
+     */
     void move(GameState.Field fieldFrom, GameState.Field fieldTo, boolean moveHalf);
 
     void sendChat(String message);
@@ -22,7 +30,14 @@ public interface Actions {
 
     void ping(GameState.Field field);
 
+    /**
+     * Remove all moves from the move queue.
+     */
     void clearMoves();
 
+    /**
+     * Get lower level API that potentially can make it possible to do something that is not possible by this interface.
+     * Use with caution, as using it can potentially make the framework work unexpectedly.
+     */
     GeneralsApi getBareApi();
 }
