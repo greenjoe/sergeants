abe2efeb57c2ae767a99de7f1ccd54ab7363a9e2
greenjoe <joegreen@joegreen.pl>
2017-02-22 23:53:54 +0100

    Additional doc for Bot interface

--- src/main/java/pl/joegreen/sergeants/framework/Bot.java
+++ src/main/java/pl/joegreen/sergeants/framework/Bot.java
@@ -8,18 +8,34 @@ import pl.joegreen.sergeants.framework.model.GameState;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public interface Bot {
+
+    /**
+     * Called on every game state update from server.
+     * @param newGameState Contains current state of the game.
+     */
     default void onGameStateUpdate(GameState newGameState) {
         getLogger(Bot.class).info("Received new game state: " + newGameState.toString());
     }
 
+    /**
+     * Called when game is won, lost or the connection to the server is lost.
+     * @param gameResult Result of the game.
+     */
     default void onGameFinished(GameResult gameResult) {
         getLogger(Bot.class).info("Game finished, result: " + gameResult);
     }
 
+    /**
+     * Called when the new game is started.
+     * @param gameStarted Contains basic info about the game (replay id, players in the game).
+     */
     default void onGameStarted(GameStarted gameStarted) {
         getLogger(Bot.class).info("Game started: " + gameStarted);
     }
 
+    /**
+     * Called on every chat message (game chat, team chat)
+     */
     default void onChatMessage(ChatMessage chatMessage){
         getLogger(Bot.class).info("Chat message: " + chatMessage);
     }
--- src/main/java/pl/joegreen/sergeants/framework/model/ChatMessage.java
+++ src/main/java/pl/joegreen/sergeants/framework/model/ChatMessage.java
@@ -9,5 +9,8 @@ public class ChatMessage {
     }
     ChatType roomType;
     String message;
+    /**
+     * Name of player sending the message. Can be null for server messages.
+     */
     String username;
 }
