4e00d9ff9ffd2d578f8f10ae5ec1a2ff528ad714
greenjoe <joegreen@joegreen.pl>
2017-04-20 22:27:25 +0200

    Simulator: Test scenario with conquering general and finishing game

--- src/test/java/pl/joegreen/sergeants/simulator/SimulatorTest.java
+++ src/test/java/pl/joegreen/sergeants/simulator/SimulatorTest.java
@@ -5,18 +5,23 @@ import pl.joegreen.sergeants.framework.model.GameState;
 import pl.joegreen.sergeants.framework.model.VisibleField;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
 public class SimulatorTest {
+
+    public static final int MANY_TURNS = 1000;
+
     @Test
     public void test2x2MapNoMovement() throws Exception {
         BotInstanceCatcher<DoNothingBot> firstBotProvider = new BotInstanceCatcher<>(DoNothingBot::new);
         BotInstanceCatcher<DoNothingBot> secondBotProvider = new BotInstanceCatcher<>(DoNothingBot::new);
         int maxTurns = 5;
         Simulator simulator = SimulatorFactory.of(new GameMap(new Tile[]{
-                new GeneralTile(0, 0), new EmptyTile(1), new EmptyTile(2), new GeneralTile(3, 1)
+                new GeneralTile(0, 0), new EmptyTile(1),
+                new EmptyTile(2), new GeneralTile(3, 1)
         }, 2, 2), maxTurns, firstBotProvider, secondBotProvider);
         simulator.start();
 
@@ -31,7 +36,8 @@ public class SimulatorTest {
     @Test
     public void test2x2CityTakeover() throws Exception {
         GameMap gameMap = new GameMap(new Tile[]{
-                new GeneralTile(0, 0), new CityTile(1, 5), new EmptyTile(2), new GeneralTile(3, 1)
+                new GeneralTile(0, 0), new CityTile(1, 5),
+                new EmptyTile(2), new GeneralTile(3, 1)
         }, 2, 2);
         BotInstanceCatcher<TestBot> conquerorBotProvider = new BotInstanceCatcher<>((actions) -> new TestBot() {
             @Override
@@ -58,6 +64,36 @@ public class SimulatorTest {
         assertEquals(2, cityArmy);
         assertEquals(turnToSecondHalfTick(maxTurns), lastGameStateReceivedByByConqueror.getTurn());
     }
+
+    @Test
+    public void test2x2GeneralTakeoverGameOver() throws Exception {
+        GameMap gameMap = new GameMap(new Tile[]{
+                new GeneralTile(0, 0), new CityTile(1, 1, Optional.of(0)),
+                new CityTile(2, 100, Optional.of(0)), new GeneralTile(3, 1)
+        }, 2, 2);
+        BotInstanceCatcher<TestBot> conquerorBotProvider = new BotInstanceCatcher<>((actions) -> new TestBot() {
+            @Override
+            public void onGameStateUpdate(GameState newGameState) {
+                super.onGameStateUpdate(newGameState);
+                if (newGameState.getTurn() == turnToFistHalfTick(1)) {
+                    actions.move(2, 3);
+                }
+            }
+        });
+        BotInstanceCatcher<DoNothingBot> doNothingBotProvider = new BotInstanceCatcher<>(DoNothingBot::new);
+
+        Simulator simulator = SimulatorFactory.of(gameMap, MANY_TURNS, conquerorBotProvider, doNothingBotProvider);
+        simulator.start();
+        List<GameState> gameStatesReceivedByConqueror = conquerorBotProvider.lastCreatedBot.receivedGameStates;
+        GameState lastGameStateReceivedByByConqueror = gameStatesReceivedByConqueror.get(gameStatesReceivedByConqueror.size() - 1);
+
+        List<VisibleField> fieldsOwnedByConqueror = lastGameStateReceivedByByConqueror.getVisibleFields().stream().filter(VisibleField::isOwnedByMe).collect(Collectors.toList());
+        assertEquals(4, fieldsOwnedByConqueror.size());;
+        int conqueredGeneralArmy = fieldsOwnedByConqueror.stream().filter(visibleField -> visibleField.getIndex() == 3).findFirst().get().getArmy();
+        assertEquals(99, conqueredGeneralArmy);
+        assertEquals(turnToSecondHalfTick(1), lastGameStateReceivedByByConqueror.getTurn());
+    }
+
 
 
     private static int turnToSecondHalfTick(int turn) {
